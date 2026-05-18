64a1d4e5270069adc73f6150c2b3d2b32a31ba61
Justin Bois <bois@caltech.edu>
2026-05-17 23:54:39 -0700

    Added activation of rachis-qiime2-2026.4 environment to updater.

--- run_update.sh
+++ run_update.sh
@@ -1,2 +1,6 @@
 # Nothing to update until we get the data
 echo "Nothing to update yet. The next update will have the class sequencing data."
+
+# Activate the rachis-qiime2-2026.4 environment
+echo "\nActivating the rachis-qiime2-2026.4 conda environment...."
+conda activate rachis-qiime2-2026.4
