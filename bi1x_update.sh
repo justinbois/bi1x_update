f7b326228014652ee841303de44f4050d187e83f
Justin Bois <bois@caltech.edu>
2026-05-17 23:08:21 -0700

    Initial commit.

---
+++ bi1x_update.sh
@@ -0,0 +1,4 @@
+cd ~/bi1x_update ;
+git pull ;
+./run_update.sh ;
+cd ~/ ;
---
+++ run_update.sh
@@ -0,0 +1,2 @@
+# Nothing to update until we get the data
+echo "Nothing to update yet. The next update will have the class sequencing data."
