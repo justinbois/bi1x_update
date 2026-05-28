6af65d99bbc2a7190994c52bdf196765c7633786
Justin Bois <bois@caltech.edu>
2026-05-28 00:20:35 -0700

    Added data set download

--- run_update.sh
+++ run_update.sh
@@ -2,13 +2,15 @@
 
 set -euo pipefail
 
-echo -e "\nDownloading sequencing data....\n\n"
-wget -O abx_sequencing.zip \
+printf '\nDownloading sequencing data....\n\n'
+
+wget -O "${HOME}/abx_sequencing.zip" \
   "https://www.dropbox.com/scl/fi/urzmjppgerw3kxebgwpf1/abx_sequencing.zip?rlkey=n6wkd3zh8862vpurbe9mqg16d&dl=1"
 
-echo -e "\nUnpacking sequencing data....\n\n"
-unzip abx_sequencing.zip
+printf '\nUnpacking sequencing data....\n\n'
 
-echo -e "\n\n**********************************************************"
-echo " Do not forget to run conda activate rachis-qiime2-2026.4"
-echo -e "**********************************************************\n\n"
+unzip "${HOME}/abx_sequencing.zip" -d "${HOME}"
+
+printf '\n\n**********************************************************\n'
+printf ' Do not forget to run conda activate rachis-qiime2-2026.4\n'
+printf '**********************************************************\n\n'
